b7b0759a795aa15c0447df4791d8d28bdf067c7d
Morgan Everec <migran1995@gmail.com>
2021-03-24 21:09:26 +0300

    добавление base view model

---
+++ viewmodels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mvvm_banking.viewmodels.Base;
+
+namespace mvvm_banking.viewmodels
+{
+    internal class MainWindowViewModel : BaseViewModel
+    {
+        
+    }
+}
