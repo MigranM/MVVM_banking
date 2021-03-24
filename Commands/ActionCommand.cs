56bff7075819cbb740b99d962318be5ead004414
Morgan Everec <migran1995@gmail.com>
2021-03-24 22:09:09 +0300

    интерфейсы для команд

---
+++ Commands/ActionCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mvvm_banking.Commands.Base;
+
+namespace mvvm_banking.Commands
+{
+    class ActionCommand : Command
+    {
+        public ActionCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        private readonly Action<object> _Execute;
+        private readonly Func<object,bool> _CanExecute;
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+
+        public override void Execute(object parameter) => _Execute(parameter);
+        
+    }
+}
--- viewmodels/MainWindowViewModel.cs
+++ viewmodels/MainWindowViewModel.cs
@@ -4,11 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using mvvm_banking.viewmodels.Base;
+using mvvm_banking.Commands;
+using System.Windows.Input;
 
 namespace mvvm_banking.viewmodels
 {
     internal class MainWindowViewModel : BaseViewModel
     {
-        
+        private string _bank_account = "10";
+
+        public ICommand Decrease_account { get; }
+
+        private bool CanDecrease_account(object p)
+        {
+            return true;
+        }
+
+        private void OnDecrease_account(object p)
+        {
+
+        }
+
+        public string bank_account
+        {
+            get => _bank_account + '₽';
+            set => Set(ref _bank_account, value);
+        }
     }
 }
